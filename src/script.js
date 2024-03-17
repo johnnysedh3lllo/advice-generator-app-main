77980b16d2e8367c4fcad6e7993b1eb36c56871e
johnnysedh3lllo <johnnysaidh3lllo@gmail.com>
2024-03-17 12:16:54 +0100

    implemented api call to render advice

--- src/script.js
+++ src/script.js
@@ -3,19 +3,28 @@ const adviceNumber = document.querySelector(".advice-card__title span");
 const adviceBlockquote = document.querySelector(".advice-card__blockquote");
 const adviceQuote = document.querySelector(".advice-card__blockquote__quote");
 const btnGenerate = document.querySelector(".btn--generate");
-const API_ENDPOINT = "https://api.adviceslip.com/advice";
+// const API_ENDPOINT = "https://api.adviceslip.com/advice";
+const API_ENDPOINT = "https://stoic-quotes.com/api/quote";
 
 console.log();
 
 async function getQuote() {
-  adviceBlockquote.innerHTML = "<p>Loading....<p/>";
+  adviceNumber.textContent = "Finding some wise guy to say some deep shit...";
+  adviceBlockquote.innerHTML = "<p>Since you asked...<p/>";
   try {
     const response = await fetch(API_ENDPOINT);
     if (!response.ok) return;
     const data = await response.json();
-    const quote = data.slip;
-    adviceNumber.textContent = quote.id;
-    adviceBlockquote.innerHTML = `<q class="advice-card__blockquote__quote">${quote.advice}</q>`;
+    console.log(data);
+
+    //advice api
+    // const quote = data.slip;
+    // adviceNumber.textContent = `From #${quote.id}`;
+    // adviceBlockquote.innerHTML = `<q class="advice-card__blockquote__quote">${quote.advice}</q>`;
+
+    // stoic quotes api
+    adviceNumber.textContent = `${data.author}`;
+    adviceBlockquote.innerHTML = `<q class="advice-card__blockquote__quote">${data.text}</q>`;
   } catch (error) {
     console.error(error);
   }
