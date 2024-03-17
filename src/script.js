35943430f110a5bebb290217a9bbac43783f69f7
johnnysedh3lllo <johnnysaidh3lllo@gmail.com>
2024-03-17 12:01:42 +0100

    implemented api call to render advice

--- src/script.js
+++ src/script.js
@@ -1,1 +1,24 @@
 "use strict";
+const adviceNumber = document.querySelector(".advice-card__title span");
+const adviceBlockquote = document.querySelector(".advice-card__blockquote");
+const adviceQuote = document.querySelector(".advice-card__blockquote__quote");
+const btnGenerate = document.querySelector(".btn--generate");
+const API_ENDPOINT = "https://api.adviceslip.com/advice";
+
+console.log();
+
+async function getQuote() {
+  adviceBlockquote.innerHTML = "<p>Loading....<p/>";
+  try {
+    const response = await fetch(API_ENDPOINT);
+    if (!response.ok) return;
+    const data = await response.json();
+    const quote = data.slip;
+    adviceNumber.textContent = quote.id;
+    adviceBlockquote.innerHTML = `<q class="advice-card__blockquote__quote">${quote.advice}</q>`;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+btnGenerate.addEventListener("click", getQuote);
